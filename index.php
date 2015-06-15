f1d1dd1bb2acdcf174368b37569b4418c98a5fb6
Shipu Ahamed <shipuahamed01@gmail.com>
2015-06-16 05:20:16 +0600

    Update index.php

--- index.php
+++ index.php
@@ -16,9 +16,13 @@ $objIntelliSMS = new IntelliSMS();
 $objIntelliSMS->Username = 'your_username';
 $objIntelliSMS->Password = 'your_password';
 
+$to_number = '8801616022666';
+$msg ='hello';
+$sender_id = '8801616022000';
+
 // For Example $objIntelliSMS->SendMessage ( '8801616022000', 'Hello iNiLABS', '8801616022666' );
 // For Multiple Numbers Send Sms: $objIntelliSMS->SendMessage ( '8801676661234,8801684761598,8801616022222', 'Hello iNiLABS', '8801616022666' );
 // +88 is Bangladesh Country Code.
-$objIntelliSMS->SendMessage ( 'to_number', 'your_message', 'Sender_ID' );
+$objIntelliSMS->SendMessage ( '$to_number', '$msg', '$sender_id' );
 
 ?>
